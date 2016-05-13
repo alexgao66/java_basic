a71dec6768eab7e58f9e4460666ac890707bcab9
gaojun <gaojun@meituan.com>
2016-05-13 11:54:38 +0800

    完善es逻辑

---
+++ src/main/java/com/alex/elasticSearch/jest/Employee.java
@@ -0,0 +1,14 @@
+package com.alex.elasticSearch.jest;
+
+import java.util.List;
+
+/**
+ * Created by gaojun on 16/5/13.
+ */
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String about;
+    private List<String> interests;
+}
